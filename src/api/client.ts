d306bbbc417a67024c4842d4d0980ff3a783a87f
Haris <haris@kutia.net>
2023-06-26 11:05:33 +0200

    clean-main

--- src/api/client.ts
+++
@@ -1,6 +0,0 @@
-import APIClient from 'api';
-import Credentials from 'api/credentials';
-
-export const credentials = new Credentials('accessToken');
-export * from 'api';
-export default new APIClient(credentials);
--- src/constants/config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-	apiUrl: import.meta.env.VITE_API_URL || window.APP_API_URL,
-};
